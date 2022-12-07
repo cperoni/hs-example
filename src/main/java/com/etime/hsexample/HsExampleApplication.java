d7dee519035a1ee5fc459ab949ca57299bd8a31c
chris <christian.peroni@arcavita.it>
2022-12-07 16:41:37 +0100

    init push

---
+++ src/main/java/com/etime/hsexample/HsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.etime.hsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etime/hsexample/HsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etime.hsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
